b7167ae81ffe3577b5a55df3148717958936b8a9
M. Nindra Zaka <mnindrazaka@gmail.com>
2022-12-29 18:22:57 +0700

    feat: setup typescript

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+type Person = {
+  name: string;
+  age: number;
+  address: string;
+};
+
+const person: Person = {
+  name: "M. Nindra Zaka",
+  age: 25,
+  address: "Probolinggo",
+};
+
+console.log(person);
